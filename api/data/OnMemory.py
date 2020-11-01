3d8155ad13623ac6ed353bc4c282f897598f8800
Leandro Funes <funes.s.leandro@gmail.com>
2020-11-01 19:50:45 -0300

    On Memory DB has locks

--- api/data/OnMemory.py
+++ api/data/OnMemory.py
@@ -1,20 +1,40 @@
 from model.Account import Account
-
+from data.ReadWriteLock import ReadWriteLock
 
 class OnMemory():
     def __init__(self):
         self._transactions = []
         self._account = Account()
+        self._lock = ReadWriteLock()
 
     def save_transaction(self, transaction):
-        self._transactions.append(transaction)
-        if transaction.type == "debit":
-            self._account.total_amount = self._account.total_amount + transaction.amount
-        else:
-            self._account.total_amount = self._account.total_amount - transaction.amount
+        self._lock.acquire_write()
+        try:
+            self._transactions.append(transaction)
+            if transaction.type == "debit":
+                self._account.register_debit(transaction.amount)
+            else:
+                self._account.register_credit(transaction.amount)
+        finally:
+            self._lock.release_write()
+        
 
     def get_all_transactions(self):
-        return self._transactions
+        self._lock.acquire_read()
+        
+        try:
+            to_ret = self._transactions
+        finally:
+            self._lock.release_read()
+
+        return to_ret 
 
     def get_account_summary(self):
-        return self._account.total_amount
+        self._lock.acquire_read()
+        
+        try:
+            to_ret = self._account.total_amount
+        finally:
+            self._lock.release_read()
+
+        return to_ret 
